5d1efefc756e199d7a508d2a9f918bf3a4474e93
leonardoattow <123214790+leonardoattow@users.noreply.github.com>
2023-12-11 12:37:51 -0400

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+// App.js
+
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <nav>
+        <ul>
+          <li><a href="#inicio">Inicio</a></li>
+          <li><a href="#acerca">Acerca de mí</a></li>
+          <li><a href="#servicios">Servicios</a></li>
+          <li><a href="#contacto">Contáctame</a></li>
+        </ul>
+      </nav>
+      <div className="carrusel">
+        hola
+      </div>
+      <div className='data'>
+      <div className='divSobreMi'>
+        <h3>UN POCO SOBRE MI</h3>
+        <p className='sobreMi'>Mi nombre es Leonardo Urdaneta tengo 22 años soy de Venezuela, desde hace año y medio comencé un viaje de autoaprendizaje como programador, gracias a mi disciplina y aprendizaje constante he logrado hacer cosas que antes parecían muy complejas para mí. He desarrollado Proyectos propios como por ejemplo esta página web, pero también he creado Varios Clones y proyectos con React como un contador de Clics y una calculadora.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+        <div className='divHabilidades'>
+          <h3>MIS HABILIDADES</h3>
+          <p className='pHabilidades'>Mi nombre es Leonardo Urdaneta tengo 22 años soy de Venezuela, desde hace año y medio comencé un viaje de autoaprendizaje como programador, gracias a mi disciplina y aprendizaje constante he logrado hacer cosas que antes parecían muy complejas para mí. He desarrollado Proyectos propios como por ejemplo esta página web, pero también he creado Varios Clones y proyectos con React como un contador de Clics y una calculadora.
+          </p>
+    </div>
+      </div>
     </div>
   );
 }
